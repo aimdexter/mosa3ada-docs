1c8f8d74932a3a9707fe0e3ecffe691d1fc4a413
aimdexter <draimade@gmail.com>
2023-10-13 23:07:07 +0100

    feat: Introduction, iterations and contributaion guidelines added

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Mosa3ada - Call For Contribution</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/BizTech-Morocco/mosa3ada.biztech.ma",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.gg/pkv9QbHJe",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://discord.gg/pkv9QbHJe",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Mosa3ada - Call For Contribution",
   },
-}
+};
 
-export default config
+export default config;
